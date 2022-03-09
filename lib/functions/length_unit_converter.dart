7cbf42065cbb0cb3c9652ac1dc048a5dd5d66066
alpha-3000 <amonkar3000@gmail.com>
2022-03-09 15:14:59 +0530

    resolving errors in length category

--- lib/functions/length_unit_converter.dart
+++ lib/functions/length_unit_converter.dart
@@ -2,82 +2,103 @@ lengthUnitConverte(firstIntialDropDownValue, secondIntialDropDownValue,
     firstTextControllerValue, secondTextControllerValue) {
   if (firstIntialDropDownValue == "centimeters" &&
       secondIntialDropDownValue == "meters") {
-    return (double.parse(firstTextControllerValue.text) / 100)
-        .toStringAsFixed(3);
+    var finalOutput =
+        (double.parse(firstTextControllerValue.text) / 100).toStringAsFixed(3);
+    return finalOutput + " " + "m";
   } else if (firstIntialDropDownValue == "centimeters" &&
       secondIntialDropDownValue == "feet") {
-    return (double.parse(firstTextControllerValue.text) / 30.48)
+    var finalOutput = (double.parse(firstTextControllerValue.text) * 1 / 30.48)
         .toStringAsFixed(3);
+    return finalOutput + " " + "ft";
   } else if (firstIntialDropDownValue == "centimeters" &&
       secondIntialDropDownValue == "kilometers") {
-    return (double.parse(firstTextControllerValue.text) / 100000)
+    var finalOutput = (double.parse(firstTextControllerValue.text) / 100000)
         .toStringAsFixed(3);
+    return finalOutput + " " + "km";
   } else if (firstIntialDropDownValue == "centimeters" &&
       secondIntialDropDownValue == "inches") {
-    return (double.parse(firstTextControllerValue.text) / 2.54)
+    var finalOutput = (double.parse(firstTextControllerValue.text) * 0.393701)
         .toStringAsFixed(3);
+    return finalOutput + " " + "inch";
   } else if (firstIntialDropDownValue == "meters" &&
       secondIntialDropDownValue == "centimeters") {
-    return (double.parse(firstTextControllerValue.text) * 100)
-        .toStringAsFixed(3);
+    var finalOutput =
+        (double.parse(firstTextControllerValue.text) * 100).toStringAsFixed(3);
+    return finalOutput + " " + "cm";
   } else if (firstIntialDropDownValue == "meters" &&
       secondIntialDropDownValue == "feet") {
-    return (double.parse(firstTextControllerValue.text) * 3.281)
+    var finalOutput = (double.parse(firstTextControllerValue.text) * 3.281)
         .toStringAsFixed(3);
+    return finalOutput + " " + "ft";
   } else if (firstIntialDropDownValue == "meters" &&
       secondIntialDropDownValue == "kilometers") {
-    return (double.parse(firstTextControllerValue.text) / 1000)
-        .toStringAsFixed(3);
+    var finalOutput =
+        (double.parse(firstTextControllerValue.text) / 1000).toStringAsFixed(3);
+    return finalOutput + " " + "km";
   } else if (firstIntialDropDownValue == "meters" &&
       secondIntialDropDownValue == "inches") {
-    return (double.parse(firstTextControllerValue.text) * 39.37)
+    var finalOutput = (double.parse(firstTextControllerValue.text) * 39.37)
         .toStringAsFixed(3);
+    return finalOutput + " " + "inch";
   } else if (firstIntialDropDownValue == "feet" &&
       secondIntialDropDownValue == "centimeters") {
-    return (double.parse(firstTextControllerValue.text) * 30.48)
+    var finalOutput = (double.parse(firstTextControllerValue.text) * 30.48)
         .toStringAsFixed(3);
+    return finalOutput + " " + "cm";
   } else if (firstIntialDropDownValue == "feet" &&
       secondIntialDropDownValue == "meters") {
-    return (double.parse(firstTextControllerValue.text) / 3.281)
+    var finalOutput = (double.parse(firstTextControllerValue.text) * 0.3048)
         .toStringAsFixed(3);
+    return finalOutput + " " + "m";
   } else if (firstIntialDropDownValue == "feet" &&
       secondIntialDropDownValue == "kilometers") {
-    return (double.parse(firstTextControllerValue.text) / 3281)
-        .toStringAsFixed(3);
+    var finalOutput =
+        (double.parse(firstTextControllerValue.text) / 3281).toStringAsFixed(6);
+    return finalOutput + " " + "km";
   } else if (firstIntialDropDownValue == "feet" &&
       secondIntialDropDownValue == "inches") {
-    return (double.parse(firstTextControllerValue.text) * 12)
-        .toStringAsFixed(3);
+    var finalOutput =
+        (double.parse(firstTextControllerValue.text) * 12).toStringAsFixed(3);
+    return finalOutput + " " + "inch";
   } else if (firstIntialDropDownValue == "kilometers" &&
       secondIntialDropDownValue == "centimeters") {
-    return (double.parse(firstTextControllerValue.text) * 100000)
+    var finalOutput = (double.parse(firstTextControllerValue.text) * 100000)
         .toStringAsFixed(3);
+    return finalOutput + " " + "cm";
   } else if (firstIntialDropDownValue == "kilometers" &&
       secondIntialDropDownValue == "meters") {
-    return (double.parse(firstTextControllerValue.text) * 1000)
-        .toStringAsFixed(3);
+    var finalOutput =
+        (double.parse(firstTextControllerValue.text) * 1000).toStringAsFixed(3);
+    return finalOutput + " " + "m";
   } else if (firstIntialDropDownValue == "kilometers" &&
       secondIntialDropDownValue == "feet") {
-    return (double.parse(firstTextControllerValue.text) * 3281)
-        .toStringAsFixed(3);
+    var finalOutput =
+        (double.parse(firstTextControllerValue.text) * 3281).toStringAsFixed(3);
+    return finalOutput + " " + "ft";
   } else if (firstIntialDropDownValue == "kilometers" &&
       secondIntialDropDownValue == "inches") {
-    return (double.parse(firstTextControllerValue.text) * 39370)
+    var finalOutput = (double.parse(firstTextControllerValue.text) * 39370)
         .toStringAsFixed(3);
+    return finalOutput + " " + "inch";
   } else if (firstIntialDropDownValue == "inches" &&
       secondIntialDropDownValue == "centimeters") {
-    return (double.parse(firstTextControllerValue.text) * 2.54)
-        .toStringAsFixed(3);
+    var finalOutput =
+        (double.parse(firstTextControllerValue.text) * 2.54).toStringAsFixed(3);
+    return finalOutput + " " + "cm";
   } else if (firstIntialDropDownValue == "inches" &&
       secondIntialDropDownValue == "meters") {
-    return (double.parse(firstTextControllerValue.text) / 39.37)
+    var finalOutput = (double.parse(firstTextControllerValue.text) / 39.37)
         .toStringAsFixed(3);
+    return finalOutput + " " + "m";
   } else if (firstIntialDropDownValue == "inches" &&
       secondIntialDropDownValue == "feet") {
-    return (double.parse(firstTextControllerValue.text) / 12)
-        .toStringAsFixed(3);
-  } else {
-    return (double.parse(firstTextControllerValue.text) / 39370)
-        .toStringAsFixed(3);
+    var finalOutput =
+        (double.parse(firstTextControllerValue.text) / 12).toStringAsFixed(3);
+    return finalOutput + " " + "ft";
+  } else if (firstIntialDropDownValue == "inches" &&
+      secondIntialDropDownValue == "kilometers") {
+    var finalOutput = (double.parse(firstTextControllerValue.text) * 0.0000254)
+        .toStringAsFixed(8);
+    return finalOutput + " " + "km";
   }
 }
