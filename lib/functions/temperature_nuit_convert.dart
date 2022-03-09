2063758ec29b3bae63730568334f32ee30412976
alpha-3000 <amonkar3000@gmail.com>
2022-03-09 11:37:46 +0530

    resolving error in temperature category

--- lib/functions/temperature_nuit_convert.dart
+++ lib/functions/temperature_nuit_convert.dart
@@ -2,26 +2,37 @@ temperatureUnitConverte(firstIntialDropDownValue, secondIntialDropDownValue,
     firstTextControllerValue, secondTextControllerValue) {
   if (firstIntialDropDownValue == "celsius" &&
       secondIntialDropDownValue == "fahrenheit") {
-    return ((double.parse(firstTextControllerValue.text) * 9 / 5) + 32)
-        .toStringAsFixed(3);
-  } else if (firstIntialDropDownValue == "fahrenheit" &&
-      secondIntialDropDownValue == "celsius") {
-    return ((double.parse(firstTextControllerValue.text) - 32) * 5 / 9)
-        .toStringAsFixed(3);
+    var finalOutput =
+        ((double.parse(firstTextControllerValue.text) * 9 / 5) + 32)
+            .toStringAsFixed(3);
+    return finalOutput + " " + "℉";
   } else if (firstIntialDropDownValue == "celsius" &&
       secondIntialDropDownValue == "kelvin") {
-    return (double.parse(firstTextControllerValue.text) + 273.15)
+    var finalOutput = (double.parse(firstTextControllerValue.text) + 273.15)
         .toStringAsFixed(3);
+    return finalOutput + " " + "°K";
+  } else if (firstIntialDropDownValue == "fahrenheit" &&
+      secondIntialDropDownValue == "celsius") {
+    var finalOutput =
+        ((double.parse(firstTextControllerValue.text) - 32) * 5 / 9)
+            .toStringAsFixed(3);
+    return finalOutput + " " + "℃";
   } else if (firstIntialDropDownValue == "fahrenheit" &&
       secondIntialDropDownValue == "kelvin") {
-    return ((double.parse(firstTextControllerValue.text) - 32) * 5 / 9 + 273.15)
-        .toStringAsFixed(3);
+    var finalOutput =
+        ((double.parse(firstTextControllerValue.text) - 32) * 5 / 9 + 273.15)
+            .toStringAsFixed(3);
+    return finalOutput + " " + "°K";
   } else if (firstIntialDropDownValue == "kelvin" &&
       secondIntialDropDownValue == "celsius") {
-    return (double.parse(firstTextControllerValue.text) - 273.15)
+    var finalOutput = (double.parse(firstTextControllerValue.text) - 273.15)
         .toStringAsFixed(3);
-  } else {
-    return ((double.parse(firstTextControllerValue.text) - 273.15) * 9 / 5 + 32)
-        .toStringAsFixed(3);
+    return finalOutput + " " + "℃";
+  } else if (firstIntialDropDownValue == "kelvin" &&
+      secondIntialDropDownValue == "fahrenheit") {
+    var finalOutput =
+        ((double.parse(firstTextControllerValue.text) - 273.15) * 9 / 5 + 32)
+            .toStringAsFixed(3);
+    return finalOutput + " " + "℉";
   }
 }
