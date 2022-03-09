ab92f87f56d6fea3932d4d2818a0f33e6e81bef6
alpha-3000 <amonkar3000@gmail.com>
2022-03-09 14:36:22 +0530

    resolving errors in volume category

--- lib/functions/volume_unit_converter.dart
+++ lib/functions/volume_unit_converter.dart
@@ -2,10 +2,14 @@ volumeUnitConverte(firstIntialDropDownValue, secondIntialDropDownValue,
     firstTextControllerValue, secondTextControllerValue) {
   if (firstIntialDropDownValue == "liters" &&
       secondIntialDropDownValue == "milliliters") {
-    return (double.parse(firstTextControllerValue.text) * 1000)
-        .toStringAsFixed(3);
-  } else {
-    return (double.parse(firstTextControllerValue.text) / 1000)
-        .toStringAsFixed(2);
+    var finalOutput =
+        (double.parse(firstTextControllerValue.text) * 1000).toStringAsFixed(3);
+    return finalOutput + " " + "mL";
+  }
+  if (firstIntialDropDownValue == "milliliters" &&
+      secondIntialDropDownValue == "liters") {
+    var finalOutput =
+        (double.parse(firstTextControllerValue.text) / 1000).toStringAsFixed(2);
+    return finalOutput + " " + "L";
   }
 }
